551fb2afaa7b545e987ecbd998c0500849d74973
ceijhey <welton.pms8@gmail.com>
2021-10-20 02:27:47 -0300

    fix conflicts

--- init/main.c
+++ init/main.c
@@ -609,7 +609,6 @@ asmlinkage __visible void __init start_kernel(void)
 	}
 
 
->>>>>>> theirs
 	parse_early_param();
 	after_dashes = parse_args("Booting kernel",
 				  static_command_line, __start___param,
